f5064c343a79c7863d06d8840758192a7d671dc7
Timothy Lee <timothy.lee-3@postgrad.manchester.ac.uk>
2026-06-03 10:33:22 +0100

    Update /mnt/iusers01/fatpou01/phy01/w00238tl/scratch/HyPER_24_2/HyPER/topology/__init__.py

--- HyPER/topology/__init__.py
+++ HyPER/topology/__init__.py
@@ -2,9 +2,11 @@ from .ttbar import ttbar_allhad
 from .ttbar import ttbar_single_lep
 from .FourTop import FourTop_allhad
 from .ttbar import ttbar_dilep
+from .tth import ttH_single_lep
 __all__ = [
     'ttbar_allhad',
     'FourTop_allhad',
     'ttbar_single_lep',
-    'ttbar_dilep'
+    'ttbar_dilep',
+    'ttH_single_lep'
 ]
